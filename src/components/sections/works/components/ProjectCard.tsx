00360ae58a1dd16d7c5b73c2badbba86d272969c
homoki-denis <denis.homoki6@gmail.com>
2025-07-28 22:37:52 +0300

    fix: project tilt

--- src/components/sections/works/components/ProjectCard.tsx
+++ src/components/sections/works/components/ProjectCard.tsx
@@ -3,10 +3,6 @@ import Image, { StaticImageData } from "next/image";
 import { motion } from "framer-motion";
 import React from "react";
 import { fadeIn } from "@/lib/motion";
-<<<<<<< HEAD
-=======
-import Tilt from "react-parallax-tilt";
->>>>>>> 49ebe7ec5bcfa92b2149a3c7d08ed4fa227da27a
 import { github } from "@/static";
 import { Tilt } from "react-tilt";
 
