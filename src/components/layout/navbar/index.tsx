3ca95272613cfe4e75cfee58b14d3c72b94eeea0
homoki-denis <denis.homoki6@gmail.com>
2025-07-28 23:11:59 +0300

    fix: menu z index

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-20 rounded-xl`}
           >
             <NavbarLinks type="mobile" />
           </div>
